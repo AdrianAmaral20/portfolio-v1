c23c0eea2b7fd702e91ec05a27e82b58a5579d88
Adrian Amaral <adri13amaral@gmail.com>
2022-07-22 10:05:27 -0300

    adicionado e estilizado o menu mobile

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ addEventListener("wheel", (event) => {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+// Botão Mobile
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
